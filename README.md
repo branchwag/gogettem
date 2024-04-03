70884e81fa7a6a3372284b83fb1c1a88463adc8e
LeAnne B <branchleannelmb@gmail.com>
2024-04-03 17:43:59 -0400

    adds in starter files

---
+++ README.md
@@ -0,0 +1,15 @@
+# GO Gettem!
+
+A template for getting started with a simple webpage using Go + echo </br>
+</br>
+
+## btw I ran these commands to get started:
+
+```
+      go mod init gogettem
+      go get github.com/labstack/echo/v4
+      go install github.com/cosmtrek/air@latest
+      go install golang.org/x/tools/gopls@latest
+      go get github.com/labstack/echo/v4/middleware
+      air
+```
---
+++ server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Templates struct {
+	templates *template.Template
+}
+
+func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func NewTemplates() *Templates {
+	return &Templates{
+		templates: template.Must(template.ParseGlob("*.html")),
+	}
+}
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+
+	e.Renderer = NewTemplates()
+
+	e.GET("/", func(c echo.Context) error {
+		//return c.String(http.StatusOK, "Hello, World!")
+		return c.Render(http.StatusOK, "index", "muhaha")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+}
